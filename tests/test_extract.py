64c5ef12da4a921433c466022fbcfa99e78d2673
Hoek <hoek@hkv.nl>
2018-08-29 14:20:17 +0200

    resolve Feature and FeatureCollection

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -3,18 +3,20 @@ import unittest
 import topojson as tj
 
 class TestExtract(unittest.TestCase):
-
     # extract copies coordinates sequentially into a buffer
-    def test_linestring(self):
+    def test_linestring(self):        
+        topology = None
         data = {
             "foo": {"type": "LineString", "coordinates": [[0, 0], [1, 0], [2, 0]]},
             "bar": {"type": "LineString", "coordinates": [[0, 0], [1, 0], [2, 0]]}
         }
         topology = tj.extract(data)
+        # print(topology)
         self.assertEqual(topology['coordinates'], [(0, 0), (1, 0), (2, 0), (0, 0), (1, 0), (2, 0)])
 
     # assess if a multipolygon is processed into the right number of rings
     def test_multipolygon(self):
+        topology = None
         data = {
             "foo": {
                 "type": "MultiPolygon",
@@ -42,10 +44,12 @@ class TestExtract(unittest.TestCase):
             }
         }
         topology = tj.extract(data)
+        # print(topology)
         self.assertEqual(len(topology['rings']), 3)  
 
     # test multiliinestring
     def test_multilinestring(self):
+        topology = None
         data = {
             "foo": {
                 "type": "MultiLineString",
@@ -57,11 +61,12 @@ class TestExtract(unittest.TestCase):
             }
         }  
         topology = tj.extract(data)
-        print(topology)
+        # print(topology)
         self.assertEqual(len(topology['lines']), 3)  
 
     # test nested geojosn geometrycollection collection
     def test_nested_geometrycollection(self):
+        topology = None
         data =  {
             "foo": {
                 "type": "GeometryCollection",
@@ -77,11 +82,12 @@ class TestExtract(unittest.TestCase):
             }
         }
         topology = tj.extract(data)
-        print(topology)
+        # print(topology)
         self.assertEqual(len(topology['objects']['foo']['geometries'][0]['geometries'][0]['arcs']), 1)         
 
     # test geometry collection + polygon
     def test_geometrycollection_polygon(self):
+        topology = None
         data = {
             "bar": {
                 "type": "Polygon",
@@ -95,11 +101,12 @@ class TestExtract(unittest.TestCase):
             }
         }
         topology = tj.extract(data)
-        print(topology)
+        # print(topology)
         self.assertEqual(len(topology['rings']), 1)  
 
     # test feature type
     def test_features(self):
+        topology = None
         data = {
             "foo": {
                 "type": "Feature", 
@@ -112,19 +119,28 @@ class TestExtract(unittest.TestCase):
         } 
              
         topology = tj.extract(data)
-        print(topology)  
+        # print(topology)  
         self.assertEqual(len(topology['rings']), 1)    
 
+    # test feature collection including geometry collection
     def test_featurecollection(self):
+        topology = None
         data = {
             "collection": {
             "type": "FeatureCollection",
             "features": [
                 {"type": "Feature", "geometry": {"type": "LineString", "coordinates": [[.1, .2], [.3, .4]]}},
-                {"type": "Feature", "geometry": {"type": "Polygon", "coordinates": [[[.5, .6], [.7, .8]]]}}
-            ]
+                {"type": "Feature", "geometry": {"type": "GeometryCollection", "geometries": [
+                    {"type": "Polygon", "coordinates": [[[0.5, 0.6], [0.7, 0.8], [0.9, 1.0]]]}]}
+                }
+            ]            
             }
         }
         topology = tj.extract(data)
-        print(topology)  
-        self.assertEqual(len(topology['rings']), 1)                     
+        # print(topology)  
+        self.assertEqual(len(topology['objects']), 2)
+        self.assertEqual(topology['coordinates'], [(0.1, 0.2), (0.3, 0.4), (0.5, 0.6), (0.7, 0.8), (0.9, 1.0), (0.5, 0.6)])
+        self.assertEqual(len(topology['rings']), 1)
+        self.assertEqual(len(topology['lines']), 1)
+        self.assertEqual(topology['objects']['feature_0']['geometries'][0]['type'], 'LineString')
+        self.assertEqual(topology['objects']['feature_1']['geometries'][0]['geometries'][0]['type'], 'Polygon')
