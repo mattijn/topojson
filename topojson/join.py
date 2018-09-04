7b306c6dbc1a5e94c4b8ce03c56846d9027744ae
Hoek <hoek@hkv.nl>
2018-09-04 09:50:58 +0200

    start of junctions polygons

--- topojson/join.py
+++ topojson/join.py
@@ -15,28 +15,26 @@ class _Join:
         self.segments = []
 
     def junctions_two_lines(self, g1, g2):
-        try:
-            forward_backward = shared_paths(g1, g2) 
-            forward = forward_backward[0]
-            backward = forward_backward[1]
+        fw_bw = shared_paths(g1, g2) 
+        if not fw_bw.is_empty:
             
-            # if both backward and forward cotains objects combine 
+            forward = fw_bw[0]
+            backward = fw_bw[1]
+
             if backward.is_empty:
+                # only contains forward objects
                 shared_segments = forward
             elif forward.is_empty:    
+                # only contains backward objects
                 shared_segments = backward       
             else:
-                # print('both full')  
+                # both backward and forward contains objects combine 
                 shared_segments = geometry.MultiLineString([
                     linemerge(forward),
                     linemerge(backward)
                 ])
 
             self.segments.extend(list(shared_segments))
-                
-        except ValueError:
-            # print('backward.is_empty and forward.is_empty')
-            pass
             
     def main(self, data):
         """
@@ -52,8 +50,17 @@ class _Join:
         https://stackoverflow.com/a/34032549
         """
         
+        # start of join from Polygon derived linearRings and LineStrings
+        
+#         # pop last coordinate        
+#         linearrings = [
+#             geometry.LineString(ring.boundary.coords[0:-1]) for ring in data['rings']
+#         ]
+        linearrings = [ring.boundary for ring in data['rings']]
+        mergerings = linearrings + data['lines']
+        
         # first create list with all combinations of lines
-        line_combs = list(itertools.combinations(data['lines'], 2))
+        line_combs = list(itertools.combinations(mergerings, 2))
         
         # iterate over line combinations
         for geoms in line_combs:
