cb318fcda75991c53b729c0ffe93218b238abe18
Mattijn van Hoek <mattijnvanhoek@Mattijns-MacBook-Pro.local>
2018-09-29 07:22:41 +0200

    start of dedup object that can remove duplicate copies

---
+++ tests/test_dedup.py
@@ -0,0 +1,28 @@
+import unittest
+import topojson    
+
+class TestDedup(unittest.TestCase): 
+    # duplicate rotated geometry bar with hole interior in geometry foo
+    def test_duplicate_rotated_hole_interior(self): 
+        data = {
+            "foo": {
+                "type": "MultiPolygon",
+                "coordinates": [
+                    [
+                        [[0, 0], [20, 0], [10, 20], [0, 0]], # CCW
+                        [[3, 2], [10, 16], [17, 2], [3, 2]] # CW
+                    ],
+                    [
+                        [[6, 4], [14, 4], [10, 12], [6, 4]] #CCW 
+                    ]
+
+                ]
+            },
+            "bar": {"type": "Polygon", "coordinates": [[[17, 2], [3, 2], [10, 16], [17, 2]]]}
+        }      
+        topo = topojson.dedup(topojson.cut(topojson.join(topojson.extract(data))))
+        # print(topo)
+        self.assertEqual(len(topo['duplicates']), 0)
+        self.assertEqual(topo['bookkeeping'], [[0, 2], [1], [2]])
+
+       
