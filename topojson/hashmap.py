f4f879a016b5628d619fd2487186d8dd2b4f54ac
Mattijn van Hoek <mattijn@gmail.com>
2019-04-06 09:25:13 +0200

    tryt to init travis

--- topojson/hashmap.py
+++ topojson/hashmap.py
@@ -78,6 +78,10 @@ class Hashmap:
         """
         Function to check if the shared arcs in geom should be backward.
         If so, are written as -(index+1)
+
+        Parameters
+        ----------
+        arc_ids :         
         """
 
         shared_bool = np.isin(arc_ids, self.data["bookkeeping_shared_arcs"])
