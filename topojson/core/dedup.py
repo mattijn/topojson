f0c6f8fe2c94c65e9aa449cc17afba3cb3180b5b
Mattijn van Hoek <mattijn@gmail.com>
2019-07-31 17:07:42 +0800

    maybe there is wrong bookkeeping in dedup for geometrycollections?

--- topojson/core/dedup.py
+++ topojson/core/dedup.py
@@ -49,6 +49,7 @@ class Dedup(Cut):
 
         # deduplicate equal geometries
         # create numpy array from bookkeeping_geoms variable for numerical computation
+        # DOUBLE CHECK WHATS HAPPENING HERE .... bookkeepings_geoms is not used here...
         array_bk = self.index_array(data["bookkeeping_linestrings"])
         array_bk_sarcs = None
         if data["bookkeeping_duplicates"].size != 0:
