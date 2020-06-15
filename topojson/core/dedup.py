b54cd4ed48304287ee172cf3017282cbf6148402
Mattijn van Hoek <mattijn@gmail.com>
2020-06-15 15:56:36 +0200

    catch no topology with dedup

--- topojson/core/dedup.py
+++ topojson/core/dedup.py
@@ -68,6 +68,10 @@ class Dedup(Cut):
         # deduplicate equal geometries
         # create numpy array from bookkeeping_geoms variable for numerical computation
         # np_array_from_lists(data["bookkeeping_linestrings"])
+
+        if not self.options.topology and data["linestrings"]:
+            data["linestrings"] = [np.asarray(d) for d in data["linestrings"]]
+
         if len(data["bookkeeping_linestrings"]):
             array_bk = np.vstack(data["bookkeeping_linestrings"])
         else:
