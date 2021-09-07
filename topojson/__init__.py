90c3e8e9eb8797587efc3e594a32fc43745ed251
mattijn <mattijn@gmail.com>
2021-09-07 23:15:06 +0200

    update repo to v1.2

--- topojson/__init__.py
+++ topojson/__init__.py
@@ -20,6 +20,6 @@ Main Features
   - Optional support for UI controls to exploring the results of topoquantize
     and toposimplify interactively if ipywidgets is installed.
 """
-__version__ = "1.1"
+__version__ = "1.2"
 
 from .core.topology import Topology  # noqa
