a2d35ba3731dd4dc94ffb023221cc601dbe36973
mattijn <mattijn@gmail.com>
2022-08-24 12:55:14 +0200

    force object_name to be list type

--- topojson/utils.py
+++ topojson/utils.py
@@ -84,9 +84,12 @@ class TopoOptions(object):
             self.winding_order = None
 
         if "object_name" in arguments:
-            self.object_name = arguments["object_name"]
+            if type(arguments["object_name"]) is not list:
+                self.object_name = [arguments["object_name"]]
+            else:
+                self.object_name = arguments["object_name"]
         else:
-            self.object_name = "data"
+            self.object_name = ["data"]
 
     def __repr__(self):
         return "TopoOptions(\n  {}\n)".format(pprint.pformat(self.__dict__))
@@ -396,12 +399,11 @@ def serialize_as_svg(topo_object, separate=False, include_junctions=False):
     from IPython.display import SVG, display
     from shapely import geometry
 
-    keys = topo_object.keys()
-    if "arcs" in keys:
+    if "arcs" in topo_object:
         arcs = topo_object["arcs"]
         if arcs:
             # dequantize if quantization is applied
-            if "transform" in keys:
+            if "transform" in topo_object:
 
                 np_arcs = np_array_from_arcs(arcs)
 
@@ -492,7 +494,7 @@ def serialize_as_geojson(
     # prepare arcs from topology object
     arcs = topo_object["arcs"]
     transform = None
-    if "transform" in topo_object.keys():
+    if "transform" in topo_object:
         transform = topo_object["transform"]
         scale = transform["scale"]
         translate = transform["translate"]
@@ -510,8 +512,8 @@ def serialize_as_geojson(
         np_arcs = np.around(np_arcs, decimals=decimals)
 
     # select object member from topology object
-    if objectname not in topo_object["objects"].keys():
-        raise SystemExit(f"'{objectname}' is not an object name in your topojson file")
+    if objectname not in topo_object["objects"]:
+        raise LookupError(f"'{objectname}' is not an object name in your topojson file")
     features = topo_object["objects"][objectname]["geometries"]
 
     # prepare geojson featurecollection
