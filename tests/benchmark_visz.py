cd1ddbb443f16e8850a3a0d73c4cab4f81b3cdf8
mattijn <mattijn@gmail.com>
2022-09-08 14:56:53 +0200

    clean up and document code

--- tests/benchmark_visz.py
+++ tests/benchmark_visz.py
@@ -4,26 +4,39 @@ import altair as alt
 import pandas as pd
 from okab.saver import OkabSaver
 
+
 def stats_to_visz():
+    # the following 3 versions are compared, namely the last released version on pypi
+    # the version that is currently on master in github and the version in the PR being
+    # processed.
+    versions = ["lastreleased", "master", "PR"]
 
-    versions = ['lastreleased', 'master', 'PR']
-    df_list = []
+    # collect all timings and create a pandas dataframe
+    list_df = []
     for version in versions:
-        fp_version = Path(f'tests/timings_{version}.json').resolve()
+        fp_version = Path(f"tests/timings_{version}.json").resolve()
         fp_version = fp_version.as_posix()
-        df_version = pd.read_json(fp_version, orient='records')
-        df_list.append(df_version)
-    df = pd.concat(df_list)
+        df_version = pd.read_json(fp_version, orient="records")
+        list_df.append(df_version)
+    df = pd.concat(list_df)
 
-    chart = alt.Chart(df, title='timing of files (s) across version', height=150).mark_circle().encode(
-        x=alt.X('version:N', title=None),
-        y=alt.Y('time:Q', title=None, scale=alt.Scale(type='log')),
-        color='version:N', 
-        column=alt.Column('file:N', title=None)
-    ).resolve_scale(x='independent')
+    # parse to altair chart
+    chart = (
+        alt.Chart(df, title="timing of files (s) across version", height=150)
+        .mark_circle()
+        .encode(
+            x=alt.X("version:N", title=None),
+            y=alt.Y("time:Q", title=None, scale=alt.Scale(type="log")),
+            color="version:N",
+            column=alt.Column("file:N", title=None),
+        )
+        .resolve_scale(x="independent")
+    )
 
-    chart.save("tests/benchmark_chart.png", method=OkabSaver, scale_factor=2)
-    
-if __name__ == '__main__':
+    # save chart as png using okab as save method
+    chart.save("tests/benchmark_chart.png", method=OkabSaver, scale_factor=1)
+
+
+if __name__ == "__main__":
 
     fire.Fire(stats_to_visz)
