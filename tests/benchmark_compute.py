d27103ad8fd1e277be6afe32a0659a4668633be3
Mattijn van Hoek <mattijn@gmail.com>
2022-09-08 15:13:48 +0200

    Continuous benchmarking (#194)
    * add new workflow
    
    * setup jobs
    
    * update actions
    
    * check listing tests dir
    
    * update path of stats file
    
    * try again
    
    * typo ..
    
    * try add commit
    
    * try resolving branch
    
    * generate benchmark chart
    
    * clean up and document code
    
    * generate benchmark chart
    
    * set scale to 2
    
    * generate benchmark chart
    
    * add dash in lastreleased
    
    * generate benchmark chart
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ tests/benchmark_compute.py
@@ -0,0 +1,52 @@
+import fire
+import json
+from time import process_time
+from pathlib import Path
+import geopandas
+import topojson
+
+
+def time_topology(data):
+    t_start = process_time()
+    _ = topojson.Topology(data)
+    t_stop = process_time()
+    return t_stop - t_start
+
+
+def time_version(version):
+    """
+    The following list contain files are being timed. The files filed should be able to
+    open by geopandas.read_file(). The characteristic of each file is as follow:
+    - nybb: a few detailed long linestrings
+    - naturalearth_lowres: similar alike linestrings, not too long
+    - mesh2d: many very short linestrings
+    """
+    files_to_time = [
+        geopandas.datasets.get_path("nybb"),
+        geopandas.datasets.get_path("naturalearth_lowres"),
+        "tests/files_geojson/mesh2d.geojson",
+    ]
+
+    # apply 3x timing to each file and collect result in list
+    list_times = []
+    for file_to_time in files_to_time:
+        gdf_data = geopandas.read_file(file_to_time)
+        for _ in range(3):
+            time_of_file = time_topology(gdf_data)
+            list_times.append(
+                {
+                    "file": Path(file_to_time).stem,
+                    "time": time_of_file,
+                    "version": version,
+                }
+            )
+
+    # save timings within test folder, these files will not be kept, but only used to
+    # create a visualization in benchmark_visz.py
+    with open(f"tests/timings_{version}.json", "w") as f:
+        json.dump(list_times, f)
+
+
+if __name__ == "__main__":
+
+    fire.Fire(time_version)
---
+++ tests/benchmark_visz.py
@@ -0,0 +1,42 @@
+import fire
+from pathlib import Path
+import altair as alt
+import pandas as pd
+from okab.saver import OkabSaver
+
+
+def stats_to_visz():
+    # the following 3 versions are compared, namely the last released version on pypi
+    # the version that is currently on master in github and the version in the PR being
+    # processed.
+    versions = ["last-released", "master", "PR"]
+
+    # collect all timings and create a pandas dataframe
+    list_df = []
+    for version in versions:
+        fp_version = Path(f"tests/timings_{version}.json").resolve()
+        fp_version = fp_version.as_posix()
+        df_version = pd.read_json(fp_version, orient="records")
+        list_df.append(df_version)
+    df = pd.concat(list_df)
+
+    # parse to altair chart
+    chart = (
+        alt.Chart(df, title="timing of files (s) across version", height=150)
+        .mark_circle()
+        .encode(
+            x=alt.X("version:N", title=None),
+            y=alt.Y("time:Q", title=None, scale=alt.Scale(type="log")),
+            color="version:N",
+            column=alt.Column("file:N", title=None),
+        )
+        .resolve_scale(x="independent")
+    )
+
+    # save chart as png using okab as save method
+    chart.save("tests/benchmark_chart.png", method=OkabSaver, scale_factor=2)
+
+
+if __name__ == "__main__":
+
+    fire.Fire(stats_to_visz)
